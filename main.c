206d67e2516252866b093d6e1f0e1da46261a512
Saimplice Ruud Christopher <69945589+Saimplice1234@users.noreply.github.com>
2020-11-16 20:05:40 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,20 @@
 #include <math.h>  
 #include <regex.h>
 #include <stdlib.h>
-
+/*
+NOM Saimplice
+PRENOM Ruud Christopher
+CLASSE NS3-B
+DATE 17 novembre 2020
+*/
 #define TAILLE_MAX 1000
 
+void pf(char * str){
+   printf("%s",str);
+}
+void reset () {
+  printf("\033[0m");
+}
 void showBannerEquation(){
 
     FILE* fichier = NULL;
@@ -13,25 +24,24 @@ void showBannerEquation(){
     if (fichier != NULL)
     {
       while (fgets(chaine, TAILLE_MAX, fichier) != NULL)
-      {
+      { 
+        printf("\033[0;33m");
         printf("%s", chaine);
+        reset();
       }
         fclose(fichier);
     }
 }
-
 int getDeltaValue(int a,int b,int c){
 
+   printf("\033[1;32m");
+   pf("Calculating ∆....\n");   
+   reset();
    int exponent=pow(b,2);
    int tempAC=a*c;
    int tempAB=4*tempAC;
    return exponent-(tempAB);
-
 }
-void pf(char * str){
-   printf("%s",str);
-}
-
 void paternTest(int value)
 {
     if (value == 0) {
@@ -44,7 +54,6 @@ void paternTest(int value)
         pf("An error occured.\n");
     }
 }
-
 void showBannerBye(){
       FILE* fichier = NULL;
     char chaine[TAILLE_MAX] = "";
@@ -52,34 +61,57 @@ void showBannerBye(){
     if (fichier != NULL)
     {
       while (fgets(chaine, TAILLE_MAX, fichier) != NULL)
-      {
+      { 
+        printf("\033[0;33m");
         printf("%s", chaine);
+        reset();
       }
         fclose(fichier);
     }
 }
 int getSolutionCase(int delta,int a,int b,int c){
   if(delta > 0){
-
+    printf("\033[1;34m");
+    pf("There are 2 double roots\n");
+    reset();
+    printf("\033[1;32m");
+    pf("Calculating x1.... \n");
+    reset();
     int x1,x2;
     int tempA=-b-delta;
     int tempB=2*a;
     x1=tempA/tempB;
-
+    pf("x1=-b-√∆/2a\n");
+    printf("x1=%d\n",x1);
+    printf("\033[1;32m");
+    pf("Calculating x2.... \n");
+    reset();
     int tempASecond=-b+delta;
     int tempBSecond=2*a;
     x2=tempASecond/tempBSecond;
-    
-    printf("L'ensemble des solutions donne S={%d:%d}",x1,x2);
+    pf("x2=-b+√∆/2a\n");
+    printf("x2=%d\n",x2);
+    pf("\033[1;32m");
+    printf("All solutions are S={%d:%d}",x1,x2);
+    reset();
     showBannerBye();
   }else if(delta<0){
-    pf("L'equation a pour solution ensemble de vide !");
+    pf("\033[1;32m");
+    pf("The equation has for solution set of vacuum S={⊘}!");
+    reset();
   }
   else if(delta == 0){
+   printf("\033[1;32m");  
+   pf("Calculating x1.... \n");
+   reset();
    int x1,x2;
    x1=-b/2*a;
    x2=x1;
-   printf("L'ensemble des solutions donne S={%d:%d}",x1,x2);
+   printf("x1=%d\n",x1);
+   pf("x1=x2\n");
+   printf("\033[1;32m");
+   printf("All solutions are S={%d:%d}\n",x1,x2);
+   reset();
   }
  return 0;
 }
@@ -91,17 +123,21 @@ int main(){
   int ptr_a_match,ptr_b_match,ptr_c_match;
 
   showBannerEquation();
+  pf("\033[1;36m");
+  pf("\n\nWelcome to our quadratic equation resolver !\n");
+  pf("A quadratic equation is presented on the form ax2+bx+c=0");
+  reset();
 
-  pf("\n\nWelcome to our quadratic equation resolver !");
-  
-  pf("\n\nEnter the a term:");
+  pf("\033[1;32m");
+  pf("\n\nEnter the number a:");
   scanf("%s",ptr_a);
 
-  pf("Enter the b term:");
+  pf("Enter the number b:");
   scanf("%s",ptr_b);
 
-  pf("Enter the c term:");
+  pf("Enter the number c:");
   scanf("%s",ptr_c);
+  reset();
 
   int match_ptr_a;
   int match_ptr_b;
@@ -128,6 +164,7 @@ int main(){
       ptr_a_match,
       ptr_b_match,
       ptr_c_match));
+      printf("∆=%d\n",delta);
       getSolutionCase(delta,ptr_a_match,ptr_b_match,ptr_c_match);
   }
   return 0;
